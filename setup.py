1faa50d713cfae3eea5991003f6fc81442067766
Brian Rosner <brosner@gmail.com>
2012-03-27 11:12:05 -0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = "pinax-utils",
+    version = "1.0b1.dev1",
+    author = "Brian Rosner",
+    author_email = "brosner@gmail.com",
+    description = "a Pinax utility app",
+    long_description = open("README.rst").read(),
+    license = "MIT",
+    url = "http://github.com/pinax/pinax-utils",
+    packages = find_packages(),
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ]
+)
